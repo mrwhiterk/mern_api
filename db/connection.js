31a3ca0c00b8111c6fbc6c5d906334e8f9debdac
Ryan White <mrwhiterk@gmail.com>
2019-03-12 20:54:44 -0400

    useMongoClient removed

--- db/connection.js
+++ db/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 if (process.env.NODE_ENV == 'production') {
-  mongoose.connect(process.env.DB_URL, { useMongoClient: true })
+  mongoose.connect(process.env.DB_URL)
 } else {
   mongoose.connect('mongodb://localhost/api-books');
 }
